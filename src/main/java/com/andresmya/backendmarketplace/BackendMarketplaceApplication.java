aed59ed80bcc13cc812485ce75bb1e0ec3330e5c
andresmya <andresmya@outlook.com>
2022-01-12 01:19:55 -0500

    Initial commit

---
+++ src/main/java/com/andresmya/backendmarketplace/BackendMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.andresmya.backendmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andresmya/backendmarketplace/BackendMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresmya.backendmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
