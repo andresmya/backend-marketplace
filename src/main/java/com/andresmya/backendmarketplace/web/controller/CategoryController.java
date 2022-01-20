1f7429ab5b9503266f2e9a0b965261ccf523cd25
andresmya <andresmya@outlook.com>
2022-01-20 01:58:27 -0500

    secure category endpoints

--- src/main/java/com/andresmya/backendmarketplace/web/controller/CategoryController.java
+++ src/main/java/com/andresmya/backendmarketplace/web/controller/CategoryController.java
@@ -4,11 +4,13 @@ import com.andresmya.backendmarketplace.domain.Category;
 import com.andresmya.backendmarketplace.domain.dto.request.create.CreateCategoryRequest;
 import com.andresmya.backendmarketplace.domain.dto.request.update.UpdateCategoryRequest;
 import com.andresmya.backendmarketplace.domain.service.CategoryService;
+import com.andresmya.backendmarketplace.domain.service.RoleService;
 import com.andresmya.backendmarketplace.exception.InvalidArgumentException;
 import com.andresmya.backendmarketplace.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -32,16 +34,19 @@ public class CategoryController {
         return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
     }
 
+    @PreAuthorize(RoleService.HAS_ROLE_ADMIN)
     @PostMapping
     public ResponseEntity<Category> createCategory(@RequestBody CreateCategoryRequest request) throws Exception {
         return new ResponseEntity<>(categoryService.createCategory(request), HttpStatus.CREATED);
     }
 
+    @PreAuthorize(RoleService.HAS_ROLE_ADMIN)
     @PatchMapping("/{id}")
     public ResponseEntity<Category> updateCategory(@PathVariable("id") Integer id, @RequestBody UpdateCategoryRequest request) throws NotFoundException, InvalidArgumentException {
         return new ResponseEntity<>(categoryService.updateCategory(id, request), HttpStatus.OK);
     }
 
+    @PreAuthorize(RoleService.HAS_ROLE_ADMIN)
     @DeleteMapping("/{id}")
     public void deleteCategoryById(@PathVariable("id") Integer id) throws NotFoundException {
         categoryService.deleteCategoryById(id);
